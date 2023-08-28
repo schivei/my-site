6395f892237c794c0a160469b70a190a18120ed0
Elton Schivei Costa <costa@elton.schivei.nom.br>
2023-08-28 14:14:23 -0300

    Add go modules and cpd to generate pdfs before publish

---
+++ cdp/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/chromedp/cdproto/page"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	task := exec.Command("hugo", "serve", "--port", "65059")
+	go func() {
+		err := task.Run()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	defer func() {
+		defer func() { _ = recover() }()
+		_ = task.Cancel()
+	}()
+
+	time.Sleep(1000)
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	fmt.Print("generating './static/resume.pdf'... ")
+
+	var resume []byte
+	if err := chromedp.Run(ctx, printToPDF(`http://localhost:65059/`, &resume)); err != nil {
+		fmt.Println()
+		log.Fatal(err)
+	}
+
+	if err := os.WriteFile("./static/resume.pdf", resume, 0o644); err != nil {
+		fmt.Println()
+		log.Fatal(err)
+	}
+
+	fmt.Println("Done")
+	fmt.Print("generating './static/curriculo.pdf'... ")
+
+	var curriculo []byte
+	if err := chromedp.Run(ctx, printToPDF(`http://localhost:65059/pt-br/`, &curriculo)); err != nil {
+		fmt.Println()
+		log.Fatal(err)
+	}
+
+	if err := os.WriteFile("./static/curriculo.pdf", curriculo, 0o644); err != nil {
+		fmt.Println()
+		log.Fatal(err)
+	}
+
+	fmt.Println("Done")
+}
+
+func printToPDF(urlStr string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlStr),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			buf, _, err := page.PrintToPDF().
+				WithPrintBackground(true).
+				WithMarginBottom(1).
+				WithMarginLeft(1).
+				WithMarginRight(1).
+				WithMarginTop(1).
+				Do(ctx)
+			if err != nil {
+				return err
+			}
+			*res = buf
+			return nil
+		}),
+	}
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module cdp
+
+go 1.21
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20230828023241-f357fd93b5d6
+	github.com/chromedp/chromedp v0.9.2
+)
+
+require (
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
